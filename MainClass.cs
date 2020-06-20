8065516ee439f3ef08fe7529a26f834f67628a5b
Alex Siegman <asiegman@gmail.com>
2020-06-19 21:15:22 -0500

    Maybe put all the close parens in

--- MainClass.cs
+++ MainClass.cs
@@ -1736,7 +1736,7 @@ namespace ES_DKP_Utils
 
         private void frmMain_FormClosing(object sender, EventArgs e)
         {
-            if (AutomaticBackups && !(Directory.Exists(BackupDirectory)) {
+            if (AutomaticBackups && !(Directory.Exists(BackupDirectory))) {
                 MessageBox.Show("Cannot automatically backup database - Backup Directory does not exist.  Please check your settings.",
                                 "Automatic DB Backup", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
