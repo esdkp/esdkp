410fbd52c894e0caaa33087d6088ee3577919764
Alex Siegman <asiegman@gmail.com>
2026-09-05 14:21:07 -0500

    Match NotPresent filter logic with AddPerson (#23)

--- Raid.cs
+++ Raid.cs
@@ -1055,7 +1055,7 @@ namespace ES_DKP_Utils
             int j = 0;
             foreach (DataRow r in p.Rows)
             {
-                if (dbRaid.Select("Name='" + r.ItemArray[0] + "'").Length > 0)
+                if (dbRaid.Select("Name='" + r.ItemArray[0] + "' AND PTS=0").Length > 0)
                 {
                     deleteMe[i] = r;
                     i++;
